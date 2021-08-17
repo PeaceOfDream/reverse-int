e25dba9074a2590d7c025a0305b1a1fa09f6839b
Dmitriy Bartashevich <3088742@mail.ru>
2021-08-17 17:11:16 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+	let num = n.toString();
+	let result = '';
+	if (num[0] == '-') {
+		num = num.slice(1);
+	}
+	for (let i = 0;i <= num.length - 1;i++) {
+		result = num[i] + result;
+	}
+	return result;
 }
